f3b5cf2f9cd09cd54c791d8cdc5bd4ef0acd0258
alsojatin <thejatinbaghell@gmail.com>
2024-07-17 11:51:39 +0530

    Create script.jsx

---
+++ script.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HelloMessage(props) {
+  const name = 'Alice'; // Assuming a name variable
+
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to React!</p>
+    </div>
+  );
+}
